38a05f45f6e063d1e189a034fc8fffa7f2952a0c
t_hirai <t_hirai@avelio.co.jp>
2025-12-23 08:29:10 +0900

    03

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-function App() {
+export const App = () => {
   return (
-    <>
-      <div>
-        Test
-      </div>
-    </>
+    <div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input type="text" value="" onChange={(e) => e.preventDefault()} />
+        <input type="submit" value="追加" onSubmit={(e) => e.preventDefault()} />
+      </form>
+    </div>
   )
 }
-
-export default App
